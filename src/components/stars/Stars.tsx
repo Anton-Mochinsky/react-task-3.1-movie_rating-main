5c7d4c43661f820233042c23a55892879519e77b
Anton <avm199615@gmail.com>
2024-08-14 17:42:39 +0300

    first commit

---
+++ src/components/stars/Stars.tsx
@@ -0,0 +1,21 @@
+import { Star } from "./Star";
+
+type StarsProps = {
+    count: number,
+}
+
+export function Stars({ count = 0 }:StarsProps) {
+    if(count <= 0 || count > 5 || typeof count !== 'number') return;
+
+    const result: Array<React.ReactNode> = [];
+
+    for (let i = 0; i < count; i += 1) {
+        result.push(<li key={i}><Star/></li>);
+    }
+
+    return (
+        <ul className="card-body-stars">
+           { result }
+        </ul>
+    )
+}
